fc89fcba9865370d8b14237a331473640feda45a
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-01-30 13:02:11 +0100

    Update create_magic_square_3x3.py

--- create_magic_square_3x3.py
+++ create_magic_square_3x3.py
@@ -8,26 +8,29 @@ def build_gameboard():
 
 def coordinate_algorithm(board):
     x, y = 1, 2
+    minimum, maximum = 0, 2
     for value in range(1, 10):
         board[x][y] = value
         x, y = x - 1, y + 1
-        if (x < 0) and (y > 2):
-            x, y = 2, 0
-        if (x < 0) and (y <= 2):
-            x, y = 2, y
-        if (x >= 0) and (y > 2):
-            x, y = x, 0
+        if (x < minimum) and (y > maximum):
+            x, y = maximum, minimum
+        if (x < minimum) and (y <= maximum):
+            x, y = maximum, y
+        if (x >= minimum) and (y > maximum):
+            x, y = x, minimum
         
         if board[x][y] != 0:
-            if (0 <= x < 2) and (y == 2):
-                x, y = x + 1, y - 2
-            elif (x == 2) and (y == 2):
-                x, y = 0, y - 2
-            elif (x == 2) and (y < 2):
-                if y == 1:
-                    x, y = 0, 2
-                elif y == 0:
-                    x, y = 0, 1
+            if (minimum <= x < maximum):
+                x = x + 1
+            elif x == maximum:
+                x = minimum
+            
+            if (2 <= y <= maximum):
+                y = y - 2
+            elif y == 1:
+                y = maximum
+            elif y == 0:
+                y = maximum - 1
         
     print('\n',*board, sep='\n')
     return board
