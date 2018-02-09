92ae43218e9f4e3e36ca8fc35327d43c11d639e5
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-02-09 14:12:25 +0100

    Update magic_square_crazy.py

--- magic_square_crazy.py
+++ magic_square_crazy.py
@@ -1,33 +1,57 @@
 import sys, string, argparse
 
-
 lst1 = [0,0,0,3,1,1,1,2]
 
 
 def main():
     option = None
+    args = cmd_options()
     action, method = input_actions_methods()
     board = build_gameboard()
-    if "c" in action:
+    if "c" in action or args.create:
         board = build_gameboard()
-    if "e" in action:
+    if "e" in action or args.edit:
         filename = "swaptest.txt" if "f" in method else None
         lst = lst1 if "s" in method else None
-        option = (swap_coordinates_from_file(filename) if "f" in method
-                 else swap_coordinates_from_lists(lst) if "s" in method
-                 else swap_values_manually() if "m" in method
+        option = (swap_coordinates_from_file(filename) if "f" in method or args.file
+                 else swap_coordinates_from_lists(lst) if "s" in method or args.sequence
+                 else swap_values_manually() if "m" in method or args.manually
                  else None)
-    check_if_magic_square = True if "i" in action else False
+    check_if_magic_square = True if "i" in action or args.inspect else False
     coordinate_algorithm(board, option)
     print_sums(board, check_if_magic_square)
 
 
 def input_actions_methods():
-    prompt_action = input("[C]reate [E]dit [I]nspect: ").lower()
-    if prompt_action in "cei":
-        prompt_method = input("[F]ile [S]eq [M]anually: ").lower()
-        if prompt_method in "fsm":
-            return prompt_action, prompt_method
+    options_msg = "[C]reate "
+    prompt_action = input(options_msg + ":").lower()
+    while True:
+        try:
+            if prompt_action in "c":
+                options_msg += "[E]dit] [I]nspect "
+                prompt_action = input(options_msg + ":").lower()
+            else:
+                raise ValueError("ERROR: Invalid input. Has to be c/C")
+            if prompt_action in "cei":
+                prompt_method = input("[F]ile [S]eq [M]anually: ").lower()
+                if prompt_method in "fsm":
+                    return prompt_action, prompt_method
+                else:
+                    raise ValueError("ERROR: Invalid input. Has to be a letter of 'fFsSmM'")
+            else:
+                raise ValueError("ERROR: Invalid input. Has to be a letter of 'cCeEiI'")
+        except ValueError as err:
+            print(err)
+
+def optionizer(method, filename, lst):
+    if "f" in method:
+        return swap_coordinates_from_file(filename)
+    elif "s" in method:
+        return swap_coordinates_from_lists(lst)
+    elif "m" in method:
+        return swap_values_manually()
+    else:
+        print("No editing methods chosen. Exiting..")
 
 
 def cmd_options():
@@ -73,48 +97,51 @@ def build_gameboard():
     return board
 
 
-def process_lists(*args):
-    lsts = []
+def process_lists(*args, items_lsts=[]):
+    remove_chars = {c for c in string.punctuation + string.whitespace + "\ufeff"}
     for lst in args:
         if not lst:
             return
-        def process_list(lst):
-            remove_chars = {c for c in string.punctuation + string.whitespace + "\ufeff"}
-            lst = [str(c) for c in lst if str(c) not in remove_chars]
-            try:
-                for element in lst:
-                    if element not in string.digits:
-                        raise ValueError("\nERROR: Has to be integers (0-9)! ")
-                if not len(lst) % 2 == 0:
-                    raise IndexError("\nERROR: Must be coordinate pair/s (x,y)!")
-                items_lst = [(lst[n], lst[n + 1]) for n in range(len(lst)) if n % 2 == 0]
-            except (ValueError, IndexError) as err:
-                print(err, "\nList {0}: not processed".format(lst))
-            else:
-                print("List {0}: OK".format(lst))
-                return items_lst
-        lst = process_list(lst)
-        lsts.append(lst)
-    return lsts if len(args) > 1 else lst
+        lst = [str(c) for c in lst if str(c) not in remove_chars]
+        try:
+            for element in lst:
+                if element not in string.digits:
+                    raise ValueError("\nERROR: Has to be integers (0-9)! ")
+            if not len(lst) % 2 == 0:
+                raise IndexError("\nERROR: Must be coordinate pair/s (x,y)!")
+            items_lst = [(lst[n], lst[n + 1]) for n in range(len(lst)) if n % 2 == 0]
+        except (ValueError, IndexError) as err:
+            print(err, "\nList {0}: not processed".format(lst))
+        else:
+            print("List {0}: OK{1}".format(lst, "\n" if len(lst) > 4 else ""))
+            if len(args) > 1:
+                items_lsts.append(items_lst)
+    return items_lsts if items_lsts else items_lst
+
+
+def in_line_coordinate_pairs():
+    prompt = input("Create coordinate pairs for swapping on each line?"
+                       "\n[default = coordinate pairs with mixed lines]: ")
+    return 0 if prompt.lower() not in {"y", "yes"} else 1
 
 
 def swap_values_manually():
-    prompt = input("Do you wish to switch some values interactively?: ")
-    if prompt.lower() not in {"y", "yes"}:
-        print("Exiting...")
-        return [], [], None
+    in_line = in_line_coordinate_pairs()
     user_friendly = user_friendly_coordinates()
     coordinates_a, coordinates_b = [], []
     a = True
     while True:
-        msg = "\nChoose x,y for {0}: ".format("A" if a else "B")
+        msg = ("\nChoose x,y for {0}: ".format("A" if a else "B") 
+                if not in_line else "\nChoose (x_A, y_A), (x_B, y_B) "
+                                    "coordinate pairs for swapping: ")
         try:
             prompt = process_lists(input(msg))
             if not prompt:
                 break
-            else:
-                coordinates = coordinates_a if a else coordinates_b
-                coordinates += prompt
+            if in_line:
+                coordinates_a, coordinates_b = prompt[::2], prompt[1::2]
+            coordinates = coordinates_a if a else coordinates_b
+            coordinates += prompt
             a = False if a else True
         except ValueError as err:
             print(err)
@@ -133,12 +160,11 @@ def swap_coordinates_from_file(filename=None):
     fh = None
     coordinates_a, coordinates_b = [], []
     try:
+        in_line = in_line_coordinate_pairs()
         fh = open(filename, encoding="utf-8")
-        prompt = input("Create coordinate pairs for swapping on each line?"
-                       "\n[default = coordinate pairs with mixed lines]: ")
         for lino, line in enumerate(fh, start=1):
             line_items = process_lists(list(line.strip()))
-            if prompt.lower() not in {"y", "yes"}:
+            if not in_line:
                 if lino % 2 != 0:
                     coordinates_a += line_items
                 else:
@@ -148,14 +174,16 @@ def swap_coordinates_from_file(filename=None):
                     if n % 2 == 0:
                         coordinates_a.append(line_items[n])
                         coordinates_b.append(line_items[n + 1])
-    except EnvironmentError as err:
+        if not (len(coordinates_a) == len(coordinates_b)):
+            raise IndexError("Must be even number of lines if pairs from mixed lines")
+    except (EnvironmentError, IndexError) as err:
         print(err)
     else:
         print("\nSuccessfully queued coordinates for swapping values\n")
+        return coordinates_a, coordinates_b, 0
     finally:
         if fh is not None:
             fh.close()
-    return coordinates_a, coordinates_b, 0
 
 
 def swap_coordinates_from_lists(coordinates_a_lst=None, 
@@ -194,7 +222,7 @@ def swap_coordinates_from_lists(coordinates_a_lst=None,
 
 
 def user_friendly_coordinates():
-    prompt = input("User-friendly coordinates (without zeros)?")
+    prompt = input("User-friendly coordinates (without zeros)?: ")
     return 1 if prompt.lower() in {"y", "yes"} else 0
 
 
@@ -225,7 +253,7 @@ def print_board(board):
         for element in row:
             print("{0:2d}".format(element), end="  ")
         print("\n")
-
+    
 
 def print_sums(board, check_if_magic_square=False):
     sums = {}
